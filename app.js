731fa7fc2f765c9924d5e5e8e28002695e6373c9
Konrad01~ <konradbc7@gmail.com>
2024-08-20 18:28:35 +0200

    added block of button when user didn't choose rating

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ const submitBtn = document.getElementById("submit-btn");
 const thanksModal = document.getElementById("thanks-side");
 const selectedRate = document.getElementById("selected-rate");
 
+const activeBtn = () => {
+	rates.forEach((rate) =>
+		rate.addEventListener("click", () => {
+			submitBtn.classList.remove("disabled");
+			submitBtn.removeAttribute("disabled");
+		})
+	);
+};
+
 const showModal = (e) => {
 	rates.forEach((rate) => {
 		if (rate.checked) {
 			selectedRate.textContent = rate.value;
+			thanksModal.classList.add("show-modal");
 		}
 	});
 
-	thanksModal.classList.add("show-modal");
 	e.preventDefault();
 };
 
+activeBtn();
 submitBtn.addEventListener("click", showModal);
